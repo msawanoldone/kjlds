c0604f0e76af278c4b80331374b707c61f617800
jsnjfndsfdsasd <40805903+ujhsfjhfsfa@users.noreply.github.com>
2018-10-07 20:01:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,7 +38,7 @@ if(message.content.startsWith(prefix + "2bc")) {
 if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
 if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
 let args = message.content.split(" ").join(" ").slice(3 + prefix.length);
-let copy = "KinGBot";
+let copy = "Bُubbles Tumblr";
 let request = `Requested By ${message.author.username}`;
 if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
 msg.react('✅')
